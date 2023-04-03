2fa50ffca0f66b4609f8cc2a77d8653cf1f66d52
yjjjwww <yjjjwww@naver.com>
2023-04-03 17:46:21 +0900

    feat : [004-PRODUCT-ORGANIZATION-ENUM] 기관 코드 Enum Class 생성

---
+++ api/src/main/kotlin/com/zerobase/api/product/model/Organization.kt
@@ -0,0 +1,9 @@
+package com.zerobase.api.product.model
+
+enum class Organization(
+        val organizationCode: String
+) {
+    NONE("00000"),
+    ORGANIZATION_ONE("00001"),
+    ORGANIZATION_TWO("00002")
+}
